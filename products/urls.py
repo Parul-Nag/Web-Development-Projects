20f3dcb47199b2f3b7f67601666676d93879f5be
Parul-Nag <nagparul1@gmail.com>
2024-12-25 20:55:18 +0530

    updated urls.py

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.ProductListView.as_view(),name='product-list'),
+    path('api/products/',views.ProductListView.as_view(),name='product-list'),
 ]
